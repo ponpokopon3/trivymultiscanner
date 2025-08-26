6827a1e49f2b25ce7442759719bd612577c28e7b
ponpokopon3 <76726254+ponpokopon3@users.noreply.github.com>
2025-08-26 13:07:37 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,5 @@
-docker run --rm -it \
+docker run --rm \
+  -e PYTHONUNBUFFERED=1 \
+  -e LOG_FILE=/app/sbom_outputs/app.log \
   -v "$PWD/sbom_outputs:/app/sbom_outputs" \
-  myapp bash -lc '
-    set -euxo pipefail
-    echo "== whoami =="; id
-    echo "== target dir inside =="
-    ls -ld /app/sbom_outputs || true
-    test -d /app/sbom_outputs
-    echo ok2 > /app/sbom_outputs/_probe2.txt
-    ls -l /app/sbom_outputs
-    cat /app/sbom_outputs/_probe2.txt
-  '
+  myapp python -X dev -u /app/app.py
